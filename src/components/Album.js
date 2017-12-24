dd3554fc1e1298357f5ede2bcae6b3705cb452cb
tr1vector <tr1vector@outlook.com>
2017-12-24 07:20:07 -0700

    Bloc Jams React Checkpoint 5 Components - Library Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-  <section className="album">
-    Album info listed here
-  </section>
-);
+//** Original function for Album page **
+// const Album = () => (
+//   <section className="album">
+//     Album info listed here
+//   </section>
+// );
+
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
 
 export default Album;

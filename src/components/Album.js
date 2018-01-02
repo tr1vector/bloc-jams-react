86b0f25dd643794cba58345419050eeaa7b955b9
tr1vector <tr1vector@outlook.com>
2018-01-02 05:42:45 -0700

    Bloc Jams React Checkpoint 6 Components - Album Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import albumData from './../data/albums';
 //** Original function for Album page **
 // const Album = () => (
 //   <section className="album">
@@ -8,10 +8,38 @@ import React, { Component } from 'react';
 // );
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+
+    const album = albumData.find( album => {
+  	  return album.slug === this.props.match.params.slug
+    });
+
+    this.state = {
+  	  album: album
+  	};
+  }
+
   render() {
     return (
       <section className="album">
-        {this.props.match.params.slug} Album will go here
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt="album cover art" />
+          <div className = "album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+          </tbody>
+        </table>
       </section>
     );
   }

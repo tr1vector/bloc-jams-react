880ae60b0d393a3e28541cc220d75bae906d6843
tr1vector <tr1vector@outlook.com>
2018-01-02 20:44:09 -0700

    Bloc Jams React Checkpoint 7 Audio Playback Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -16,8 +16,38 @@ class Album extends Component {
     });
 
     this.state = {
-  	  album: album
+  	  album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
   	};
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song })
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
@@ -40,22 +70,18 @@ class Album extends Component {
           <tbody>
           {
             this.state.album.songs.map( (song, index) =>
-            	song.element = (
-              	  <tr>
-			        <td>
-			          <button>
-				          <span class="song-number">{index + 1}</span>
-				          <span class="ion-play"></span>
-				          <span class="ion-pause"></span>
-				        </button>
-			        </td>
-
-			        <td className="song-title">{song.title}</td>
-			        <td className="song-duration">{song.duration}</td>
-			      </tr>
-			    )
-            )
-		  }
+            	<tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+			          <td className="song-actions">
+			            <button>
+				            <span class="song-number">{index + 1}</span>
+				            <span class="ion-play"></span>
+				            <span class="ion-pause"></span>
+				          </button>
+			          </td>
+			          <td className="song-title">{song.title}</td>
+			          <td className="song-duration">{song.duration}</td>
+			        </tr>
+            )}
           </tbody>
         </table>
       </section>

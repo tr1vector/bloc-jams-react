699b0b04f509e8ce0e13286df91bec1eded55384
tr1vector <tr1vector@outlook.com>
2018-02-23 08:24:27 -0700

    Bloc Jams React User Story Player Buttons - Next Button Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -59,6 +59,14 @@ class Album extends Component {
     this.play(newSong);
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = currentIndex +1;
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
   render() {
     return (
       <section className="album">
@@ -98,6 +106,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
